7d68c41fd605264515ec814f2eb11be404eb023f
HassanRaza45 <muhammadhassan.bsaif21@iba-suk.edu.pk>
2023-11-06 12:54:16 +0500

    add some files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import {HeroSection} from './components/HeroSection';
+import Footer from './components/Footer';
+function App(){
+    return(
+        <div>
+            {/*JSX */}
+        <Header/>
+        <HeroSection/>
+        <Footer/>
+
+        </div>
+    );
+    export default App;
+}
