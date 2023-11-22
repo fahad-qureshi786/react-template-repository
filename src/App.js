717051a076102a33cf50b4210de8bc92de599896
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2023-11-22 16:25:13 +0500

    create or modify components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import {HeroSection} from './components/HeroSection';
 import Footer from './components/Footer';
-function App(){
-    return(
+import {Route, Routes} from "react-router-dom";
+import HeroSection from "./components/HeroSection";
+
+export default function App() {
+    return (
         <div>
             {/*JSX */}
-        <Header/>
-        <HeroSection/>
-        <Footer/>
+            <Header/>
+                <Routes>
+                    <Route path={"/"} element={<HeroSection />}></Route>
+                </Routes>
+            <Footer/>
 
         </div>
     );
-    export default App;
 }
